0a55d19c65bbf227b19107edcf653c68c131fcfc
Lucas Reppe Welander <lucaswelanderx@gmail.com>
2024-09-05 14:04:54 +0200

    updates

---
+++ lua/lrw/remap.lua
@@ -0,0 +1,10 @@
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Format code
+vim.keymap.set("n", "<leader>b", "<cmd>!biome format --fix %<CR>")
