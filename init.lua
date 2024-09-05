2c626bd29f5b9acc2b5c29b1dab43c915d5dd605
Lucas Reppe Welander <lucaswelanderx@gmail.com>
2024-09-05 22:07:38 +0200

    removed more nvchad

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
@@ -11,30 +10,10 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
 
 -- load plugins
 require("lazy").setup({
-    {
-        "NvChad/NvChad",
-        lazy = false,
-        branch = "v2.5",
-        import = "nvchad.plugins",
-    },
-
-    { import = "plugins" },
     { import = "lrw.plugins" },
-}, lazy_config)
-
--- load theme
-dofile(vim.g.base46_cache .. "defaults")
-dofile(vim.g.base46_cache .. "statusline")
-
-require "options"
-require "nvchad.autocmds"
-
-vim.schedule(function()
-    require "mappings"
-end)
+})
 
 require "lrw"
